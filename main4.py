7ee332ad5fadb1d605db504e310659dcbca4e3f8
renegadelha <renegadelha@gmail.com>
2023-09-30 15:50:10 -0300

    new

---
+++ main4.py
@@ -0,0 +1,14 @@
+#faça um algoritmo em python para repor as perdas com inflação anual (IPCA=4,5%) por 10 anos. Ao final de cada ano,
+#o programa deve dizer quanto foi o valor total de salários recebidos pelo funcionário.
+
+salario = float(input('digite seu salario'))
+
+
+for ano in range(2024, 2035):
+    novo_salario = round((salario * 13) * 1.045, 2)
+    mensal = round(novo_salario / 13, 2)
+
+    print(f'no ano de {ano} eu recebi ao total {novo_salario}, ao mês fiquei recebendo {mensal}')
+    salario = mensal
+
+
