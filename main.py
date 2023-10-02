b730ea2c70cd5d4b44029f52f8f3a911f7b209b7
renegadelha <renegadelha@gmail.com>
2023-10-02 06:59:30 -0300

    novabranch

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 #contar quantas vogais existem no nome da pessoa
+#asldkaçlskd
 
-nome = input('digite o seu nome')
+nome = input('digite o seu nome MODIF')
 quantidade = 0
 
 for letra in nome:
     if(letra == 'a' or letra == 'e' or letra == 'i' or letra == 'o' or letra == 'u'):
         quantidade = quantidade + 1
 
-print(f'a quantidade de vogais no nome foi {quantidade}')
+print(f'----------------a quantidade de vogais no nome foi {quantidade}')
